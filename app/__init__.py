d636f0bfb7cf9f77e7ba4169b1a9ab371b05628f
Moody Rahman <moody.byte@gmail.com>
2021-02-27 13:18:42 -0500

    API demo

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
-
-# from google.cloud import language_v1
+from google.cloud import language_v1
 from flask import *
 
 app = Flask(__name__)
+client = language_v1.LanguageServiceClient()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/api", methods=["POST", "GET"])
+def nlpapi():
+    if request.method == "POST":
+        text = request.form["text"]
+        document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+        # Detects the sentiment of the text
+        sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+
+        print("Text: {}".format(text))
+        return render_template("nlpdemo.html", sentiment=sentiment, text=text)
+    else:
+        return render_template("nlpdemo.html")
+
