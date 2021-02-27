7b7e98f05186feb3e0ee482a29f62555345d29db
Moody Rahman <moody.byte@gmail.com>
2021-02-27 16:24:47 -0500

    syntax analysis demo

--- app/__init__.py
+++ app/__init__.py
@@ -22,3 +22,17 @@ def nlpapi():
     else:
         return render_template("nlpdemo.html")
 
+
+@app.route("/syntaxapi", methods=["POST", "GET"])
+def syntaxapi():
+    if request.method == "POST":
+        text = request.form["text"]
+        document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+        # Detects the sentiment of the text
+        tokens = client.analyze_syntax(request={'document': document})
+
+        print("Text: {}".format(text))
+        return render_template("syntaxdemo.html", tokens=tokens, text=text)
+    else:
+        return render_template("syntaxdemo.html")
